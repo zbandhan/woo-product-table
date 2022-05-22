a938e62ee67e37aa2ecd05f859c752457d9eafea
Fazle Bari <fazlebarisn@gmail.com>
2022-05-22 16:30:02 +0600

    Almost done

--- includes/items/variations_name.php
+++ includes/items/variations_name.php
@@ -1,1 +1,13 @@
-hell
+<?php
+ 
+if ( $product->get_type() == 'variable' ) {
+    foreach ( $product->get_available_variations() as $key ) {
+        $attr_strings = array();
+        foreach ( $key['attributes'] as $attr_name => $attr_value ) {
+            $attr_strings[] = $attr_value;
+        }
+        //echo '<br/>' . implode( ', ', $attr_string );
+        $attr_string = implode( ', ', $attr_strings );
+        echo '<br/>' . ucfirst ($attr_string );
+    }
+}
