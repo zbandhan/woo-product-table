66ff59f18c21124777f46074dc0bf4aebcafc37c
Saiful Islam <codersaiful@gmail.com>
2022-07-26 14:20:57 +0600

    working

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -156,6 +156,7 @@ if( ! function_exists( 'wpt_shortcode_generator' ) ){
             $minicart_position = $basics['minicart_position'] ?? '';//isset( $basics['ajax_action'] ) ? $basics['ajax_action'] : false;
             $table_class = $basics['table_class'] ?? '';//isset( $basics['ajax_action'] ) ? $basics['ajax_action'] : false;
             $temp_number = $ID;//Temp Number Has REmoved Totally $basics['temp_number'];// + $ID; //$ID has removed from temp_number
+            var_dump($basics);
             $add_to_cart_text = $basics['add_to_cart_text'] ?? '';
             $responsive = isset( $basics['responsive'] ) ? $basics['responsive'] : 'no_responsive';
             $add_to_cart_selected_text = isset( $basics['add_to_cart_selected_text'] ) ? $basics['add_to_cart_selected_text'] : __( 'Add to cart selected', 'wpt_pro' );
